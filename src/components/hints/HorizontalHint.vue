19d388c8a47740e7f31290b1914364c2af847534
NoCodeNoBugs@gmail.com <Mikhail Vazhenin>
2021-01-08 22:19:08 +0300

    horizontal hints (in development)

---
+++ src/components/hints/HorizontalHint.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex">
+    <v-card>
+      <slot name="left"> </slot>
+    </v-card>
+    <v-card>
+      <slot name="middle"> </slot>
+    </v-card>
+    <v-card>
+      <slot name="right"> </slot>
+    </v-card>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class HorizontalHint extends Vue {}
+</script>
